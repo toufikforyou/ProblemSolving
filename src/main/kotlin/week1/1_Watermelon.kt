846031fbe06a006f698935eb539979ee1f0ba2fc
MH TOUFIK <toufikforyou@gmail.com>
2025-01-21 21:17:20 +0600

    initial commit

---
+++ src/main/kotlin/week1/1_Watermelon.kt
@@ -0,0 +1,11 @@
+package week1
+
+fun main() {
+    val weight: Int = readln().toInt()
+
+    if (weight % 2 == 0 && weight > 2) {
+        println("YES")
+    } else {
+        println("NO")
+    }
+}
---
+++ src/main/kotlin/week1/2_Way_Too_Long_Words.kt
@@ -0,0 +1,15 @@
+package week1
+
+fun main() {
+    val number: Int = readln().toInt()
+
+    for (n in 1..number) {
+        val input = readln()
+        val first = input.first()
+        val last = input.last()
+        val length = input.length
+        val usedLength = length - 2
+
+        if (length <= 10) println(input) else println("$first$usedLength$last")
+    }
+}
